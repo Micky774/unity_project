36cdd1c489a862e33deca81f27a38d967a898697
AnonymousFool <qwyner@comcast.net>
2023-04-24 16:06:26 -0400

    Restructured HealthBar code for memory efficiency

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,16 +9,33 @@ using UnityEngine.UI;
  */
 public class HealthBar : MonoBehaviour
 {
+    // Variable values assigned in Inspector
     public Robo player;
-    public Heart[] hearts;
-    public int max_hearts;
-    public Sprite filled_heart;
-    public Sprite empty_heart;
+    public GameObject heart_prefab;
+
+    // Variables instantiated during runtime
+    public Heart[] hearts = new Heart[50];
+    
+    [Range(1, 10)]
+    public int max_hearts = 5;
+
     
     void Start() {
-        max_hearts = Mathf.Clamp(player.max_health, 0, hearts.Length);
+        if(max_hearts > hearts.Length) {
+            max_hearts = hearts.Length;
+        }
+
+        // Populates heart array up to index max_hearts
+        for(int idx = 0; idx < max_hearts; idx++) {
+            hearts[idx] = (
+                Instantiate(heart_prefab, Vector3.zero, Quaternion.identity, transform)
+                .GetComponent<Heart>()
+                .Init(idx)
+            );
+        }
     }
     
+
     
     public void UpdateHealthBar(){
         // Ensures player's health variable never exceeds the max number of hearts in use
@@ -28,10 +45,10 @@ public class HealthBar : MonoBehaviour
 
         // Sets heart sprites to match player health
         for (int i = 0; i < player.curr_health; i++) {
-            hearts[i].GetComponent<Image>().sprite = filled_heart;
+            hearts[i].SetMode(HEART_MODE.active);
         }
         for (int i = player.curr_health; i < max_hearts; i++) {
-            hearts[i].GetComponent<Image>().sprite = empty_heart;
+            hearts[i].SetMode(HEART_MODE.inactive);
         }
     }
 }
--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-/*
- * A single heart on the health bar
- * No scripts currently necessary here, just need C# file for health bar functionality.
- */
 public class Heart : MonoBehaviour
 {
+    public Sprite filled_heart;
+    public Sprite empty_heart;
+    public Heart Init(int idx) {
+        name = "Heart" + idx;
+        GetComponent<RectTransform>().anchoredPosition = new Vector3(50f + 100f*idx, 0f, 0f);
+        return this;
+    }
+    public void SetMode(HEART_MODE mode) {
+        switch(mode){
+            case HEART_MODE.active:
+                GetComponent<Image>().sprite = filled_heart;
+                break;
+            case HEART_MODE.inactive:
+                GetComponent<Image>().sprite = empty_heart;
+                break;
+            case HEART_MODE.invisible:
+                GetComponent<CanvasRenderer>().SetAlpha(0f);
+                return;
+        } 
+        GetComponent<CanvasRenderer>().SetAlpha(1f);
+    }
+}
+
+public enum HEART_MODE{
+    active,
+    inactive,
+    invisible,
 }
