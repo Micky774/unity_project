d3f365f8d3177a63fb3c825a56e451863eeca4f1
AnonymousFool <qwyner@comcast.net>
2023-05-01 13:29:24 -0400

    Renamed Enemy to Bomb and added empty abstract class Enemy.cs extending Monobehaviour and extended by Bomb.cs

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : Enemy
+{
+
+    private Rigidbody2D myRigidbody;
+    private SpriteRenderer mySprite;
+    public Rigidbody2D player;
+    // Start is called before the first frame update
+    public float max_speed = 4;
+    public float acceleration_rate = 1;
+    private void Awake(){
+        myRigidbody = GetComponent<Rigidbody2D>();
+        mySprite = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Used to normalize updates across various frame-rates
+        float time_step = Time.fixedDeltaTime;
+
+        // Constructs a unit-vector along one of the eight digital directions
+        Vector2 acceleration = player.position - myRigidbody.position;
+        mySprite.flipX = acceleration.x <= 0;
+        acceleration *= time_step * acceleration_rate / acceleration.magnitude;
+        myRigidbody.velocity = Vector2.ClampMagnitude(myRigidbody.velocity + acceleration, max_speed);
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,34 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-
-    private Rigidbody2D myRigidbody;
-    private SpriteRenderer mySprite;
-    public Rigidbody2D player;
-    // Start is called before the first frame update
-    public float max_speed = 4;
-    public float acceleration_rate = 1;
-    private void Awake(){
-        myRigidbody = GetComponent<Rigidbody2D>();
-        mySprite = GetComponent<SpriteRenderer>();
-    }
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Used to normalize updates across various frame-rates
-        float time_step = Time.fixedDeltaTime;
-
-        // Constructs a unit-vector along one of the eight digital directions
-        Vector2 acceleration = player.position - myRigidbody.position;
-        mySprite.flipX = acceleration.x <= 0;
-        acceleration *= time_step * acceleration_rate / acceleration.magnitude;
-        myRigidbody.velocity = Vector2.ClampMagnitude(myRigidbody.velocity + acceleration, max_speed);
-    }
 }
