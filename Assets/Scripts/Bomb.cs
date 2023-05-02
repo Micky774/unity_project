b52ed4079f9dc1cf742d1cd65ab7debf37e1d1f7
AnonymousFool <qwyner@comcast.net>
2023-05-02 14:28:12 -0400

    Made a draft implementation of Enemy abstract class

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Bomb : Enemy
 {
-    private const float MAX_SPEED = 4;
-    private const float ACCELERATION_RATE = 1;
+    private const float _MAX_SPEED = 4;
+    private const float _ACCELERATION_RATE = 1;
+    private Rigidbody2D _myRigidBody, _player;
 
-    protected override void Start()
+    protected override void Start(){
+        _myRigidBody = this.GetComponent<Rigidbody2D>();
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        
+        this._idleBehaviour = new DoNothing(this, ENEMY_STATE.idle);
+        this._awareBehaviour = new ApproachTarget(this, _player, _MAX_SPEED, _ACCELERATION_RATE, ENEMY_STATE.aware);
+        this._engagedBehaviour = new ApproachTarget(this, _player, _MAX_SPEED, _ACCELERATION_RATE, ENEMY_STATE.engaged);
+    }
+
+    protected override void ChangeState()
     {
-        Rigidbody2D player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
-        MoveBehaviour myMoveBehaviour = new FollowPlayer(this, player, MAX_SPEED, ACCELERATION_RATE);
-        this.myCombatBehaviour = new JustMove(myMoveBehaviour);
+        float dist_to_player = (_myRigidBody.position - _player.position).magnitude;
+        if(dist_to_player < 50){
+            this._state = ENEMY_STATE.engaged;
+        }
+        else if (dist_to_player < 100){
+            this._state = ENEMY_STATE.aware;
+        }
+        else{
+            this._state = ENEMY_STATE.idle;
+        }
+
+        Debug.Log("My distance to the player is " + dist_to_player + " so my current enemy state is " + this._state);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,12 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : MonoBehaviour
-{
-    protected CombatBehaviour myCombatBehaviour;
+/*
+ * Abstract class providing framework for enemy script design
+ */
+public abstract class Enemy : MonoBehaviour{
 
+    protected EnemyBehaviour _idleBehaviour, _awareBehaviour, _engagedBehaviour;
+    protected ENEMY_STATE _state = ENEMY_STATE.idle;
+    protected bool _can_change_state = true;
     protected abstract void Start();
-    protected void Update(){
-        myCombatBehaviour.Act();
+    protected virtual void Update(){
+        if(this._can_change_state){
+            this.ChangeState();
+        }
+        this._can_change_state = this.PerformAction();
+    }
+    protected abstract void ChangeState();
+    protected virtual bool PerformAction(){
+        switch(this._state){
+            case ENEMY_STATE.idle:
+                return _idleBehaviour.Act();
+            case ENEMY_STATE.aware:
+                return _awareBehaviour.Act();
+            case ENEMY_STATE.engaged:
+                return _engagedBehaviour.Act();
+            default:
+                return true;
+        }
     }
 }
