49e2cbd7ede46e5039dc14a954f2b142e5d7f716
AnonymousFool <qwyner@comcast.net>
2023-05-03 15:41:55 -0400

    Added error checking, comments, and serialization

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Bomb : Enemy
 {
+    // These are only initial values for behaviour construction
     private const float _MAX_SPEED = 4;
     private const float _ACCELERATION_RATE = 1;
-    private Rigidbody2D _myRigidBody, _player;
+    
+    private Rigidbody2D _player;
 
+    // Bomb does nothing while idle, approaches player otherwise
     protected override void Start(){
-        _myRigidBody = this.GetComponent<Rigidbody2D>();
-        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        this._myRigidBody = this.GetComponent<Rigidbody2D>();
+        this._player = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         
         this._idleBehaviour = new DoNothing(this, ENEMY_STATE.idle);
-        this._awareBehaviour = new ApproachTarget(this, _player, _MAX_SPEED, _ACCELERATION_RATE, ENEMY_STATE.aware);
-        this._engagedBehaviour = new ApproachTarget(this, _player, _MAX_SPEED, _ACCELERATION_RATE, ENEMY_STATE.engaged);
+        this._awareBehaviour = new ApproachTarget(this, _player, Bomb._MAX_SPEED, Bomb._ACCELERATION_RATE, ENEMY_STATE.aware);
+        this._engagedBehaviour = new ApproachTarget(this, _player, Bomb._MAX_SPEED, Bomb._ACCELERATION_RATE, ENEMY_STATE.engaged);
     }
 
+    // Bomb aware of player within 100 units, engaged within 50.
     protected override void ChangeState()
     {
-        float dist_to_player = (_myRigidBody.position - _player.position).magnitude;
+        float dist_to_player = (this._myRigidBody.position - this._player.position).magnitude;
         if(dist_to_player < 50){
             this._state = ENEMY_STATE.engaged;
         }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,18 +6,33 @@ using UnityEngine;
  * Abstract class providing framework for enemy script design
  */
 public abstract class Enemy : MonoBehaviour{
-
+    // Each enemy state has a corresponding behaviour the enemy performs while in that state
+    [SerializeReference]
     protected EnemyBehaviour _idleBehaviour, _awareBehaviour, _engagedBehaviour;
+    // We assume by default that every enemy is initially unaware of the player
     protected ENEMY_STATE _state = ENEMY_STATE.idle;
+    // By initializing this as true, we ensure an enemy can change state on its first frame
     protected bool _can_change_state = true;
+
+    // We assume all enemies will have a Rigidbody2D for collision purposes
+    protected Rigidbody2D _myRigidBody;
+
+    // We enforce that an enemy define a Start method to initialize its enemy-specific behaviours
     protected abstract void Start();
+
+    // We perform updates to enemies on FixedUpdate since they are physics objects
+    // FixedUpdate is virtual to allow overriding by custom non-state-based enemies (such as bosses, potentially)
     protected virtual void FixedUpdate(){
         if(this._can_change_state){
             this.ChangeState();
         }
         this._can_change_state = this.PerformAction();
     }
+
+    // We currently enforce that an enemy define a ChangeState method to determine the enemy-specific conditions under which it becomes idle, aware, and engaged.
     protected abstract void ChangeState();
+
+    // PerformAction is virtual to allow overriding by custom non-state-based enemies (such as bosses, potentially)
     protected virtual bool PerformAction(){
         switch(this._state){
             case ENEMY_STATE.idle:
@@ -26,7 +41,9 @@ public abstract class Enemy : MonoBehaviour{
                 return _awareBehaviour.Act();
             case ENEMY_STATE.engaged:
                 return _engagedBehaviour.Act();
+            // In practice, this condition check should never be reached
             default:
+                System.Console.WriteLine("WARNING: Enemy unexpectedly missing state.");
                 return true;
         }
     }
